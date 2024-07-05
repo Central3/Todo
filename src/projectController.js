a41aa7430c271f44866f292cbd9c7badebb3cc12
Central3 <pawarrsumukha@gmail.com>
2024-07-05 16:27:07 +0530

    add localStorage support for persisting and deleting tasks

--- src/projectController.js
+++ src/projectController.js
@@ -1,9 +1,51 @@
 import { nanoid } from "nanoid";
 import project from "./project";
+import store from "./storage";
 
+const storage = store();
 let projects = [];
 
 export const getProjects = () => projects;
+
+export const loadProjects = () => {
+    const storedProjects = storage.getItem("projects");
+
+    if (storedProjects) {
+        const parsedProjects = JSON.parse(storedProjects);
+        projects = parsedProjects.map((item) => {
+            const proj = project(item.id, item.name);
+            item.tasks.forEach((task) =>
+                proj.addTask(
+                    task.title,
+                    task.desc,
+                    task.dueDate,
+                    task.priority,
+                    task.done,
+                    task.id
+                )
+            );
+            return proj;
+        });
+    }
+};
+
+loadProjects();
+
+export const saveProjects = () => {
+    const serializeProjects = projects.map((item) => ({
+        id: item.id,
+        name: item.name,
+        tasks: item.getTasks().map((task) => ({
+            id: task.id,
+            title: task.title,
+            desc: task.desc,
+            dueDate: task.dueDate,
+            priority: task.priority,
+            done: task.done,
+        })),
+    }));
+    storage.storeItem("projects", JSON.stringify(serializeProjects));
+};
 
 export const getProjectById = (id) => {
     const found = projects.find((project) => project.id === id);
